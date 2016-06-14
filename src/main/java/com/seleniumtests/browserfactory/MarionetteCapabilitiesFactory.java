1d00c05d91aba188300a6d0ddbacf8b7bc763901
bhecquet <bhecquet.w@free.fr>
2016-06-14 11:30:53 +0200

    correction in profile deployment

--- src/main/java/com/seleniumtests/browserfactory/MarionetteCapabilitiesFactory.java
+++ src/main/java/com/seleniumtests/browserfactory/MarionetteCapabilitiesFactory.java
@@ -14,27 +14,16 @@
 package com.seleniumtests.browserfactory;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.Paths;
 
 import org.openqa.selenium.Platform;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.seleniumtests.core.SeleniumTestsContext;
-import com.seleniumtests.core.TestLogging;
-
-import com.seleniumtests.customFFprofile.FireFoxProfileMarker;
-
 import com.seleniumtests.driver.DriverConfig;
-
 import com.seleniumtests.helper.FileUtility;
 import com.seleniumtests.helper.OSUtility;
-
-import net.jsourcerer.webdriver.jserrorcollector.JavaScriptError;
 
 public class MarionetteCapabilitiesFactory extends FirefoxCapabilitiesFactory {
 
--- tools/launch.bat
+++ tools/launch.bat
@@ -1,5 +1,5 @@
 CD /d "%~dp0"
 
-rem example: java -cp seleniumtestsframework.jar;plugins\googleTest-tests.jar org.testng.TestNG data\googleTest\testng\testng_google2.xml -testnames tnr
-java -cp seleniumtestsframework.jar;plugins\%app%-tests.jar org.testng.TestNG %* 
+rem example: java -cp seleniumRobot.jar;plugins\googleTest-tests.jar org.testng.TestNG data\googleTest\testng\testng_google2.xml -testnames tnr
+java -cp seleniumRobot.jar;plugins\%app%-tests.jar org.testng.TestNG %* 
 EXIT /B %ERRORLEVEL%
