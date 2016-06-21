b17a77d432324a70c7fd0cd199b95f1b3437c9c0
bhecquet <bhecquet@seleniumRobot.com>
2016-06-21 15:09:07 +0200

    Add scenario exception to allow specifying that operation order is wrong

---
+++ src/main/java/com/seleniumtests/customexception/ScenarioException.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 www.infotel.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.seleniumtests.customexception;
+
+/**
+ * Error raised by test when scenario is wrong (e.g: access a protected resource while not connected)
+ * @author behe
+ *
+ */
+public class ScenarioException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public ScenarioException(final String message, final Throwable throwable) {
+        super(message, throwable);
+    }
+    public ScenarioException(final String message) {
+    	super(message);
+    }
+
+}
