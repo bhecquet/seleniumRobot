52ab618cd6db2d5e86fba79aab61531cad41d923
Sotine <sophie.ledos@gmail.com>
2016-07-06 14:41:41 +0200

    basics csv tests

--- src/main/java/com/seleniumtests/reporter/SeleniumTestsReporter.java
+++ src/main/java/com/seleniumtests/reporter/SeleniumTestsReporter.java
@@ -16,7 +16,6 @@ package com.seleniumtests.reporter;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
