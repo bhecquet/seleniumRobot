19d9d5da73fe8f095b72bac54907d848f92ef3b8
bhecquet <bhecquet@seleniumRobot.com>
2016-10-14 13:47:17 +0200

    Add tests class

---
+++ src/test/java/com/seleniumtests/it/reporter/StubTestClass4.java
@@ -0,0 +1,38 @@
+/**
+ * Orignal work: Copyright 2015 www.seleniumtests.com
+ * Modified work: Copyright 2016 www.infotel.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.seleniumtests.it.reporter;
+
+import org.testng.annotations.Test;
+
+public class StubTestClass4 {
+
+	/**
+	 * Return formated Javadoc of the specified method
+	 * @param
+	 * @return
+	 */	
+	
+	@Test(groups="stub")
+	public void test1() {
+		
+	}
+	
+	@Test(dependsOnMethods={"test1"}, description="This is a description of this method")
+	public void test2() {
+
+	}
+}
