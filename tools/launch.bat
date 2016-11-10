375619d6ba4495b5b2c306c98d408e684a674ce2
behe <bhecquet.w@free.fr>
2016-11-10 16:50:38 +0100

    remove old structure

--- tools/launch.bat
+++
@@ -1,5 +0,0 @@
-CD /d "%~dp0"
-
-rem example: java -cp seleniumRobot.jar;plugins\googleTest-tests.jar org.testng.TestNG data\googleTest\testng\testng_google2.xml -testnames tnr
-java -cp seleniumRobot.jar;plugins\%app%-tests.jar org.testng.TestNG %* 
-EXIT /B %ERRORLEVEL%
