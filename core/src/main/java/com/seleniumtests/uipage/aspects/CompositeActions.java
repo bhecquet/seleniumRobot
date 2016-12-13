6590f4777d2f06fc656e1aafe68e6d8631621c2f
bhecquet <bhecquet@seleniumRobot.com>
2016-12-13 10:19:57 +0100

    unit test correction

--- core/src/main/java/com/seleniumtests/uipage/aspects/CompositeActions.java
+++ core/src/main/java/com/seleniumtests/uipage/aspects/CompositeActions.java
@@ -1,9 +1,10 @@
 package com.seleniumtests.uipage.aspects;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.openqa.selenium.interactions.Action;
@@ -11,10 +12,6 @@ import org.openqa.selenium.interactions.ClickAction;
 import org.openqa.selenium.interactions.CompositeAction;
 import org.openqa.selenium.support.ui.SystemClock;
 
-import com.seleniumtests.core.SeleniumTestsContextManager;
-import com.seleniumtests.customexception.ConfigurationException;
-import com.seleniumtests.customexception.DatasetException;
-import com.seleniumtests.customexception.ScenarioException;
 import com.seleniumtests.driver.CustomEventFiringWebDriver;
 import com.seleniumtests.driver.WebUIDriver;
 import com.seleniumtests.util.helper.WaitHelper;
@@ -40,16 +37,23 @@ public class CompositeActions {
 	 * and clicks in CompositeAction
 	 * Capture is done on all Action sub-classes, else it would never be done
 	 * @param joinPoint
+	 * @throws SecurityException 
+	 * @throws NoSuchFieldException 
+	 * @throws IllegalAccessException 
+	 * @throws IllegalArgumentException 
 	 */
 	@Before("call(public void org.openqa.selenium.interactions.Action+.perform ())")
-	public void updateHandles(JoinPoint joinPoint) {
+	public void updateHandles(JoinPoint joinPoint) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		if (!(joinPoint.getTarget() instanceof CompositeAction)) {
 			return;
 		}
 		CompositeAction compositeAction = (CompositeAction)joinPoint.getTarget();
+		Field actionListField = CompositeAction.class.getDeclaredField("actionsList");
+		actionListField.setAccessible(true);
+		List<Action> actionsList = (List<Action>)actionListField.get(compositeAction);
 		
 		boolean clickRequested = false;
-		for (Action action: compositeAction.asList()) {
+		for (Action action: actionsList) {
 			if (action instanceof ClickAction) {
 				clickRequested = true;
 			}
