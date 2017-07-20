ba4842435b09fa5f49e493083dcc8aeb66454012
bhecquet <bhecquet@seleniumRobot.com>
2017-07-20 10:09:42 +0200

    Connector to snapshot server finished

--- core/src/main/java/com/seleniumtests/connectors/selenium/SeleniumRobotSnapshotServerConnector.java
+++ core/src/main/java/com/seleniumtests/connectors/selenium/SeleniumRobotSnapshotServerConnector.java
@@ -1,15 +1,23 @@
 package com.seleniumtests.connectors.selenium;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.BaseRequest;
+import com.mashape.unirest.request.body.MultipartBody;
 import com.seleniumtests.core.SeleniumTestsContextManager;
+import com.seleniumtests.customexception.ConfigurationException;
 import com.seleniumtests.driver.BrowserType;
 
 public class SeleniumRobotSnapshotServerConnector extends SeleniumRobotServerConnector {
@@ -20,11 +28,15 @@ public class SeleniumRobotSnapshotServerConnector extends SeleniumRobotServerCon
 	private static final String SESSION_API_URL = "/api/session/";
 	private static final String TESTCASE_API_URL = "/api/testcase/";
 	private static final String TESTSTEP_API_URL = "/api/teststep/";
+	private static final String SNAPSHOT_API_URL = "/upload/image";
 	private Integer applicationId;
 	private Integer versionId;
 	private Integer environmentId;
 	private Integer sessionId;
+	private String sessionUUID;
 	private Integer testCaseId;
+	private Integer testStepId;
+	private Integer snapshotId;
 
 	public SeleniumRobotSnapshotServerConnector() {
 		super();
@@ -41,11 +53,8 @@ public class SeleniumRobotSnapshotServerConnector extends SeleniumRobotServerCon
 			return;
 		}
 		try {
-			JSONObject applicationJson = Unirest.post(url + APPLICATION_API_URL)
-					.field("name", SeleniumTestsContextManager.getApplicationName())
-					.asJson()
-					.getBody()
-					.getObject();
+			JSONObject applicationJson = getJSonResponse(Unirest.post(url + APPLICATION_API_URL)
+					.field("name", SeleniumTestsContextManager.getApplicationName()));
 			applicationId = applicationJson.getInt("id");
 		} catch (UnirestException | JSONException e) {
 			logger.error("cannot create application", e);
@@ -60,12 +69,9 @@ public class SeleniumRobotSnapshotServerConnector extends SeleniumRobotServerCon
 			createApplication();
 		}
 		try {
-			JSONObject versionJson = Unirest.post(url + VERSION_API_URL)
+			JSONObject versionJson = getJSonResponse(Unirest.post(url + VERSION_API_URL)
 					.field("name", SeleniumTestsContextManager.getApplicationVersion())
-					.field("application", applicationId)
-					.asJson()
-					.getBody()
-					.getObject();
+					.field("application", applicationId));
 			versionId = versionJson.getInt("id");
 		} catch (UnirestException | JSONException e) {
 			logger.error("cannot create version", e);
@@ -77,11 +83,8 @@ public class SeleniumRobotSnapshotServerConnector extends SeleniumRobotServerCon
 			return;
 		}
 		try {
-			JSONObject envJson = Unirest.post(url + ENVIRONMENT_API_URL)
-					.field("name", SeleniumTestsContextManager.getThreadContext().getTestEnv())
-					.asJson()
-					.getBody()
-					.getObject();
+			JSONObject envJson = getJSonResponse(Unirest.post(url + ENVIRONMENT_API_URL)
+					.field("name", SeleniumTestsContextManager.getThreadContext().getTestEnv()));
 			environmentId = envJson.getInt("id");
 		} catch (UnirestException | JSONException e) {
 			logger.error("cannot create environment", e);
@@ -101,16 +104,14 @@ public class SeleniumRobotSnapshotServerConnector extends SeleniumRobotServerCon
 		try {
 			BrowserType browser = SeleniumTestsContextManager.getThreadContext().getBrowser();
 			browser = browser == null ? BrowserType.NONE : browser;
+			sessionUUID = UUID.randomUUID().toString();
 			
-			JSONObject sessionJson = Unirest.post(url + SESSION_API_URL)
-					.field("sessionId", UUID.randomUUID().toString())
+			JSONObject sessionJson = getJSonResponse(Unirest.post(url + SESSION_API_URL)
+					.field("sessionId", sessionUUID)
 					.field("date", LocalDate.now().format(DateTimeFormatter.ISO_DATE))
 					.field("browser", browser.getBrowserType())
 					.field("environment", SeleniumTestsContextManager.getThreadContext().getTestEnv())
-					.field("version", versionId)
-					.asJson()
-					.getBody()
-					.getObject();
+					.field("version", versionId));
 			sessionId = sessionJson.getInt("id");
 		} catch (UnirestException | JSONException e) {
 			logger.error("cannot create session", e);
@@ -121,21 +122,182 @@ public class SeleniumRobotSnapshotServerConnector extends SeleniumRobotServerCon
 	 * Create test case and add it to the current session
 	 */
 	public void createTestCase(String testName) {
+		if (!active) {
+			return;
+		}
 		if (versionId == null) {
 			createVersion();
 		}
 		try {
-			JSONObject envJson = Unirest.post(url + TESTCASE_API_URL)
+			JSONObject testJson = getJSonResponse(Unirest.post(url + TESTCASE_API_URL)
 					.field("name", testName)
-					.field("version", versionId)
-					.asJson()
-					.getBody()
-					.getObject();
-			testCaseId = envJson.getInt("id");
+					.field("version", versionId));
+			testCaseId = testJson.getInt("id");
+			addCurrentTestCaseToSession();
 		} catch (UnirestException | JSONException e) {
 			logger.error("cannot create test case", e);
 		}
 	}
+	
+
+	/**
+	 * Create test step and add it to the current test case
+	 */
+	public void createTestStep(String testStep) {
+		if (!active) {
+			return;
+		}
+		try {
+			JSONObject stepJson = getJSonResponse(Unirest.post(url + TESTSTEP_API_URL)
+					.field("name", testStep));
+			testStepId = stepJson.getInt("id");
+			addCurrentTestStepToTestCase();
+		} catch (UnirestException | JSONException e) {
+			logger.error("cannot create test step", e);
+		}
+	}
+	
+	/**
+	 * Create snapshot
+	 */
+	public void createSnapshot(File pictureFile) {
+		if (!active) {
+			return;
+		}
+		if (sessionId == null) {
+			createSession();
+		}
+		if (testStepId == null && testCaseId == null) {
+			throw new ConfigurationException("Test step and test case must be previously defined");
+		}
+		try {
+			snapshotId = null;
+			getJSonResponse(Unirest.post(url + SNAPSHOT_API_URL)
+					.field("step", testStepId)
+					.field("sessionId", sessionUUID)
+					.field("testCase", testCaseId)
+					.field("image", pictureFile)
+					);
+			snapshotId = 0; // for test only
+		} catch (UnirestException | JSONException e) {
+			logger.error("cannot create test snapshot", e);
+		}
+	}
+	
+	public List<String> getTestListFromSession() {
+		if (sessionId == null) {
+			return new ArrayList<>();
+		}
+		
+		try {
+			
+			JSONObject sessionJson = getJSonResponse(Unirest.get(url + SESSION_API_URL + sessionId));
+			return sessionJson.getJSONArray("testCases")
+					.toList()
+					.stream()
+					.map(Object::toString)
+					.collect(Collectors.toList());
+			
+		} catch (UnirestException | JSONException e) {
+			logger.error("cannot get test case list", e);
+		}
+		return new ArrayList<>();
+	}
+
+	/**
+	 * Add the current test case (should have been previously created) to this test session
+	 */
+	public void addCurrentTestCaseToSession() {
+		if (sessionId == null || testCaseId == null) {
+			throw new ConfigurationException("Session and Test case must be previously created");
+		}
+		
+		try {
+			// get list of tests associated to this session
+			List<String> testCases = getTestListFromSession();
+			
+			MultipartBody request = Unirest.patch(url + SESSION_API_URL + sessionId + "/")
+					.field("testCases", testCaseId);
+			
+			// add already linked test cases, except the current one
+			for (String tc: testCases) {
+				if (testCaseId.toString().equals(tc)) {
+					continue;
+				}
+				request = request.field("testCases", tc);
+			}
+			getJSonResponse(request);
+		} catch (UnirestException | JSONException e) {
+			logger.error("cannot create test case", e);
+		}
+	}
+	
+	/**
+	 * Returns list of test steps in a test case
+	 * @return
+	 */
+	public List<String> getStepListFromTestCase() {
+		if (testCaseId == null) {
+			return new ArrayList<>();
+		}
+		
+		try {
+
+			JSONObject sessionJson = getJSonResponse(Unirest.get(url + TESTCASE_API_URL + testCaseId));
+			return sessionJson.getJSONArray("testSteps")
+					.toList()
+					.stream()
+					.map(Object::toString)
+					.collect(Collectors.toList());
+
+		} catch (UnirestException | JSONException e) {
+			logger.error("cannot get test step list", e);
+		}
+		return new ArrayList<>();
+	}
+	
+	/**
+	 * Add the current test case (should have been previously created) to this test session
+	 */
+	public void addCurrentTestStepToTestCase() {
+		if (testStepId == null || testCaseId == null) {
+			throw new ConfigurationException("Test step and Test case must be previously created");
+		}
+		
+		try {
+			// get list of tests associated to this session
+			List<String> testSteps = getStepListFromTestCase();
+			
+			MultipartBody request = Unirest.patch(url + TESTCASE_API_URL + testCaseId + "/")
+					.field("testSteps", testStepId);
+			
+			// add already linked test steps, except the current one
+			for (String ts: testSteps) {
+				if (testStepId.toString().equals(ts)) {
+					continue;
+				}
+				request = request.field("testSteps", ts);
+			}
+			getJSonResponse(request);
+		} catch (UnirestException | JSONException e) {
+			logger.error("cannot create test case", e);
+		}
+	}
+	
+	private JSONObject getJSonResponse(BaseRequest request) throws UnirestException {
+		HttpResponse<String> response = request.asString();
+		
+		if (response.getStatus() >= 400) {
+			throw new UnirestException(String.format("request to %s failed: %s", request.getHttpRequest().getUrl(), response.getStatusText()));
+		}
+		
+		if (response.getStatus() == 204) {
+			return new JSONObject();
+		}
+		
+		return new JSONObject(response.getBody());
+	}
+	
 
 	public int getApplicationId() {
 		return applicationId;
@@ -155,5 +317,13 @@ public class SeleniumRobotSnapshotServerConnector extends SeleniumRobotServerCon
 
 	public Integer getTestCaseId() {
 		return testCaseId;
+	}
+
+	public Integer getTestStepId() {
+		return testStepId;
+	}
+
+	public Integer getSnapshotId() {
+		return snapshotId;
 	}
 }
