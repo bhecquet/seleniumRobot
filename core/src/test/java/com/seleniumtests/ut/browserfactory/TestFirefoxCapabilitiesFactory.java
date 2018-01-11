ad4e2ccd6331f850a1b5c118ed5f89ed2015c986
bhecquet <bhecquet@seleniumRobot.com>
2018-01-11 18:33:43 +0100

    issue #56: added unit tests for FirefoxCapabilitiesFactory

--- core/src/test/java/com/seleniumtests/ut/browserfactory/TestFirefoxCapabilitiesFactory.java
+++ core/src/test/java/com/seleniumtests/ut/browserfactory/TestFirefoxCapabilitiesFactory.java
@@ -16,27 +16,48 @@
  */
 package com.seleniumtests.ut.browserfactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.seleniumtests.MockitoTest;
+import com.seleniumtests.browserfactory.BrowserInfo;
 import com.seleniumtests.browserfactory.FirefoxCapabilitiesFactory;
-import com.seleniumtests.core.proxy.ProxyConfig;
+import com.seleniumtests.driver.BrowserType;
 import com.seleniumtests.driver.DriverConfig;
+import com.seleniumtests.util.osutility.OSUtility;
 
+@PrepareForTest({OSUtility.class})
 public class TestFirefoxCapabilitiesFactory extends MockitoTest {
 
 	@Mock
 	private DriverConfig config;
-	
+
 	@Mock
-	private ProxyConfig proxyConfig;
+	private Proxy proxyConfig;
+	
+
+	@BeforeMethod(groups= {"ut"})
+	public void init() {
+		Map<BrowserType, BrowserInfo> browserInfos = new HashMap<>();
+		browserInfos.put(BrowserType.FIREFOX, new BrowserInfo(BrowserType.FIREFOX, "58.0", "/usr/bin/firefox"));
+		PowerMockito.mockStatic(OSUtility.class);
+		PowerMockito.when(OSUtility.getInstalledBrowsersWithVersion()).thenReturn(browserInfos);
+		PowerMockito.when(OSUtility.getCurrentPlatorm()).thenReturn(Platform.WINDOWS);
+	}
 	
 	/**
 	 * Check default behaviour
@@ -44,34 +65,144 @@ public class TestFirefoxCapabilitiesFactory extends MockitoTest {
 	@Test(groups={"ut"})
 	public void testCreateDefaultCapabilities() {
 		
-		Mockito.when(config.isUseFirefoxDefaultProfile()).thenReturn(false);
 		Mockito.when(config.isEnableJavascript()).thenReturn(true);
-		Mockito.when(config.getProxyConfig()).thenReturn(proxyConfig);
+		Mockito.when(config.getProxy()).thenReturn(proxyConfig);
 		
-		DesiredCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
+		MutableCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
 		
 		Assert.assertTrue(capa.is(CapabilityType.SUPPORTS_JAVASCRIPT));
+		Assert.assertTrue(capa.is(CapabilityType.TAKES_SCREENSHOT));
+		Assert.assertTrue(capa.is(CapabilityType.ACCEPT_SSL_CERTS));
 		Assert.assertEquals(capa.getVersion(), "");
+		Assert.assertEquals(capa.getCapability(CapabilityType.PROXY), proxyConfig);
 	}
 	
 	@Test(groups={"ut"})
-	public void testCreateCapabilitiesWithProfileInfo() {
+	public void testCreateDefaultCapabilitiesWithPlatform() {
 		
-		Mockito.when(config.isUseFirefoxDefaultProfile()).thenReturn(false);
+		Mockito.when(config.isEnableJavascript()).thenReturn(true);
+		Mockito.when(config.getProxy()).thenReturn(proxyConfig);
+		Mockito.when(config.getWebPlatform()).thenReturn(Platform.WINDOWS);
+		
+		MutableCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
+		
+		Assert.assertEquals(capa.getPlatform(), Platform.WINDOWS);
+		
+	}
+	
+	@Test(groups={"ut"})
+	public void testCreateDefaultCapabilitiesWithJavascriptDisabled() {
+		
 		Mockito.when(config.isEnableJavascript()).thenReturn(false);
-		Mockito.when(config.getProxyConfig()).thenReturn(proxyConfig);
-		Mockito.when(config.getBrowserDownloadDir()).thenReturn("/home");
-		Mockito.when(config.getNtlmAuthTrustedUris()).thenReturn("http://home.com");
-		Mockito.when(config.getUserAgentOverride()).thenReturn("");
-		Mockito.when(config.getFirefoxBinPath()).thenReturn("/home/bin");
+		Mockito.when(config.getProxy()).thenReturn(proxyConfig);
 		
-		DesiredCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
+		MutableCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
 		
 		Assert.assertFalse(capa.is(CapabilityType.SUPPORTS_JAVASCRIPT));
-		Assert.assertEquals(((FirefoxProfile)capa.getCapability(FirefoxDriver.PROFILE)).getIntegerPreference("browser.download.folderList", 0), 2);
-		Assert.assertEquals(System.getProperty("webdriver.firefox.bin"), "/home/bin");
-		Assert.assertEquals(((FirefoxProfile)capa.getCapability(FirefoxDriver.PROFILE)).getStringPreference("general.useragent.override", "_"), "");
-		Assert.assertEquals(((FirefoxProfile)capa.getCapability(FirefoxDriver.PROFILE)).getStringPreference("network.automatic-ntlm-auth.trusted-uris", ""), "http://home.com");
+		
+	}
+	
+	@Test(groups={"ut"})
+	public void testCreateDefaultCapabilitiesWithVersion() {
+		
+		Mockito.when(config.isEnableJavascript()).thenReturn(true);
+		Mockito.when(config.getProxy()).thenReturn(proxyConfig);
+		Mockito.when(config.getBrowserVersion()).thenReturn("60.0");
+		
+		MutableCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
+		
+		Assert.assertEquals(capa.getVersion(), "60.0");
+		
+	}
+	
+	@Test(groups={"ut"})
+	public void testCreateDefaultFirefoxCapabilities() {
+		
+		Mockito.when(config.isSetAcceptUntrustedCertificates()).thenReturn(true);
+		Mockito.when(config.isSetAssumeUntrustedCertificateIssuer()).thenReturn(true);
+		
+		MutableCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
+		
+		Assert.assertEquals(capa.getCapability(CapabilityType.BROWSER_NAME), "firefox");
+		Assert.assertEquals(capa.getCapability(FirefoxDriver.MARIONETTE), false);
+		
+		FirefoxProfile profile = (FirefoxProfile)capa.getCapability(FirefoxDriver.PROFILE);
+		
+		// check profile
+		Assert.assertTrue(profile.getBooleanPreference("webdriver_accept_untrusted_certs", false));
+		Assert.assertTrue(profile.getBooleanPreference("webdriver_assume_untrusted_issuer", false));
+		Assert.assertEquals(profile.getStringPreference("capability.policy.default.Window.QueryInterface", ""), FirefoxCapabilitiesFactory.ALL_ACCESS);
+		Assert.assertEquals(profile.getStringPreference("capability.policy.default.Window.frameElement.get", ""), FirefoxCapabilitiesFactory.ALL_ACCESS);
+		Assert.assertEquals(profile.getStringPreference("capability.policy.default.HTMLDocument.compatMode.get", ""), FirefoxCapabilitiesFactory.ALL_ACCESS);
+		Assert.assertEquals(profile.getStringPreference("capability.policy.default.Document.compatMode.get", ""), FirefoxCapabilitiesFactory.ALL_ACCESS);
+		Assert.assertEquals(profile.getIntegerPreference("dom.max_chrome_script_run_time", 100), 0);
+		Assert.assertEquals(profile.getIntegerPreference("dom.max_script_run_time", 100), 0);
+	}
+	
+	@Test(groups={"ut"})
+	public void testCreateFirefoxCapabilitiesOverrideUserAgent() {
+		
+		Mockito.when(config.getUserAgentOverride()).thenReturn("FIREFOX 55");
+		
+		MutableCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
+		
+		FirefoxProfile profile = (FirefoxProfile)capa.getCapability(FirefoxDriver.PROFILE);
+		
+		// check profile
+		Assert.assertEquals(profile.getStringPreference("general.useragent.override", ""), "FIREFOX 55");
+	}
+	
+	@Test(groups={"ut"})
+	public void testCreateFirefoxCapabilitiesOverrideBinPath() {
+		try {
+			Mockito.when(config.getFirefoxBinPath()).thenReturn("/opt/firefox/bin/firefox");
+			
+			MutableCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
+			
+			Assert.assertEquals(System.getProperty("webdriver.firefox.bin"), "/opt/firefox/bin/firefox");
+		} finally {
+			System.clearProperty("webdriver.firefox.bin");
+		}
+	}
+	
+	@Test(groups={"ut"})
+	public void testCreateFirefoxCapabilitiesStandardBinPath() {
+		try {
+			MutableCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
+	
+			Assert.assertEquals(System.getProperty("webdriver.firefox.bin"), "/usr/bin/firefox");
+		} finally {
+			System.clearProperty("webdriver.firefox.bin");
+		}
+	}
+	
+	@Test(groups={"ut"})
+	public void testCreateFirefoxCapabilitiesOverrideNtlmAuth() {
+		
+		Mockito.when(config.getNtlmAuthTrustedUris()).thenReturn("uri://uri.ntlm");
+		
+		MutableCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
+		
+		FirefoxProfile profile = (FirefoxProfile)capa.getCapability(FirefoxDriver.PROFILE);
+		
+		// check profile
+		Assert.assertEquals(profile.getStringPreference("network.automatic-ntlm-auth.trusted-uris", ""), "uri://uri.ntlm");
+	}
+	
+	@Test(groups={"ut"})
+	public void testCreateFirefoxCapabilitiesOverrideDownloadDir() {
+		
+		Mockito.when(config.getBrowserDownloadDir()).thenReturn("/home/download");
+		
+		MutableCapabilities capa = new FirefoxCapabilitiesFactory().createCapabilities(config);
+		
+		FirefoxProfile profile = (FirefoxProfile)capa.getCapability(FirefoxDriver.PROFILE);
+		
+		// check profile
+		Assert.assertEquals(profile.getStringPreference("browser.download.dir", ""), "/home/download");
+		Assert.assertEquals(profile.getIntegerPreference("browser.download.folderList", 0), 2);
+		Assert.assertEquals(profile.getBooleanPreference("browser.download.manager.showWhenStarting", true), false);
+		Assert.assertEquals(profile.getStringPreference("browser.helperApps.neverAsk.saveToDisk", ""), "application/octet-stream,text/plain,application/pdf,application/zip,text/csv,text/html");
 	}
 	
 }
