b40e7cf1234fa7a90296060aaa989119025277d7
Bertrand <bhecquet.w@free.fr>
2018-01-14 11:24:38 +0100

    issue #56: adapt unit tests for linux

--- core/src/main/java/com/seleniumtests/util/osutility/OSUtility.java
+++ core/src/main/java/com/seleniumtests/util/osutility/OSUtility.java
@@ -44,7 +44,7 @@ public abstract class OSUtility {
 
 	private String[] webDriverProcessList = {"chromedriver", "geckodriver", "IEDriverServer", "MicrosoftWebDriver"};
 		
-	private static Map<BrowserType, BrowserInfo> installedBrowsersWithVersion = OSUtilityFactory.getInstance().discoverInstalledBrowsersWithVersion();
+	private static Map<BrowserType, BrowserInfo> installedBrowsersWithVersion;
 	
 	/******************************************
 	 *********** OS information ***************
@@ -365,6 +365,9 @@ public abstract class OSUtility {
     }
 
 	public static Map<BrowserType, BrowserInfo> getInstalledBrowsersWithVersion() {
+		if (installedBrowsersWithVersion == null) {
+			refreshBrowserList();
+		}
 		return installedBrowsersWithVersion;
 	}
 	
