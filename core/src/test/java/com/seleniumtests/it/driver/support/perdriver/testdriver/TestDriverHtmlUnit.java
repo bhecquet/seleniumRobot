92d646fccfcf415d98bbfd3675754dd4260b6b82
bhecquet <bhecquet@seleniumRobot.com>
2018-05-25 08:42:18 +0200

    issue #76: various corrections
    disable some HtmlUnit tests where driver is not compatible

--- core/src/test/java/com/seleniumtests/it/driver/support/perdriver/testdriver/TestDriverHtmlUnit.java
+++ core/src/test/java/com/seleniumtests/it/driver/support/perdriver/testdriver/TestDriverHtmlUnit.java
@@ -1,5 +1,7 @@
 package com.seleniumtests.it.driver.support.perdriver.testdriver;
 
+import org.testng.annotations.Test;
+
 import com.seleniumtests.driver.BrowserType;
 import com.seleniumtests.it.driver.TestDriver;
 
@@ -7,6 +9,41 @@ public class TestDriverHtmlUnit extends TestDriver {
 
 	public TestDriverHtmlUnit() throws Exception {
 		super(BrowserType.HTMLUNIT);
+	}
+	
+	@Test(groups={"it"})
+	public void testClickActionCheckbox() {
+		// skip as htmlunit does not support it
+	}
+	
+	@Test(groups={"it"})
+	public void testAutoScrolling() {
+		// skip as htmlunit does not support it
+	}
+	
+	@Test(groups={"it"})
+	public void testClickActionDiv() {
+		// skip as htmlunit does not support it
+	}
+	
+	@Test(groups={"it"})
+	public void testClickActionRadio() {
+		// skip as htmlunit does not support it
+	}
+	
+	@Test(groups={"it"})
+	public void testDoubleClickActionDiv() {
+		// skip as htmlunit does not support it
+	}
+	
+	@Test(groups={"it"})
+	public void testUploadFileWithRobot() {
+		// skip as htmlunit does not support it
+	}
+	
+	@Test(groups={"it"})
+	public void testUploadFileWithRobotKeyboard() {
+		// skip as htmlunit does not support it
 	}
 
 }
