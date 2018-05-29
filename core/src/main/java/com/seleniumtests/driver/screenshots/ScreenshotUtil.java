a3d2dbbd9a07a3f11dcddae549c91724683eab54
bhecquet <bhecquet@seleniumRobot.com>
2018-05-29 13:34:42 +0200

    issue #144: revert parts of 8c962a96dba53d65fcbb5eda50672e4392468a88

--- core/src/main/java/com/seleniumtests/driver/screenshots/ScreenshotUtil.java
+++ core/src/main/java/com/seleniumtests/driver/screenshots/ScreenshotUtil.java
@@ -36,6 +36,7 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 
 import com.seleniumtests.core.SeleniumTestsContextManager;
+import com.seleniumtests.customexception.ConfigurationException;
 import com.seleniumtests.customexception.ScenarioException;
 import com.seleniumtests.customexception.WebSessionEndedException;
 import com.seleniumtests.driver.BrowserType;
@@ -292,7 +293,12 @@ public class ScreenshotUtil {
 			throw new ScenarioException("Desktop capture can only be done on Desktop tests");
 		}
 
-		String screenshotString = CustomEventFiringWebDriver.captureDesktopToBase64String();
+		if (driver == null) {
+			throw new ConfigurationException("driver is null, capture cannot be done");
+		}
+		
+		// use driver because, we need remote desktop capture when using grid mode
+		String screenshotString = (String)((CustomEventFiringWebDriver)driver).executeScript(CustomEventFiringWebDriver.NON_JS_CAPTURE_DESKTOP);
 		 
 		try {
 			if (screenshotString != null && !screenshotString.isEmpty()) {
