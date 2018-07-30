8e0384016e4d435b5ca2e316eb655ef34b9e0a53
s047432 <s047432@SN782980.societe.mma.fr>
2018-07-30 17:30:47 +0200

    update license in file headers

--- core/src/test/java/com/seleniumtests/it/driver/support/perdriver/testframe/TestFrameHtmlUnit.java
+++ core/src/test/java/com/seleniumtests/it/driver/support/perdriver/testframe/TestFrameHtmlUnit.java
@@ -1,3 +1,21 @@
+/**
+ * Orignal work: Copyright 2015 www.seleniumtests.com
+ * Modified work: Copyright 2016 www.infotel.com
+ * 				Copyright 2017 B.Hecquet
+ * 				Copyright 2018 Covea
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.seleniumtests.it.driver.support.perdriver.testframe;
 
 import com.seleniumtests.driver.BrowserType;
