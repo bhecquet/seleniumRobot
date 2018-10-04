62c524883f6f41e232a75b47d0c0d9892b8b2eec
bhecquet <bhecquet@seleniumRobot.com>
2018-10-04 16:00:23 +0200

    issue #191: add capability to communicate expected tools

--- core/src/main/java/com/seleniumtests/browserfactory/SeleniumRobotCapabilityType.java
+++ core/src/main/java/com/seleniumtests/browserfactory/SeleniumRobotCapabilityType.java
@@ -3,4 +3,5 @@ package com.seleniumtests.browserfactory;
 public interface SeleniumRobotCapabilityType {
 
 	String NODE_TAGS = "nodeTags"; // value must be a list of strings
+	String TOOLS = "tools"; // value must be a list of strings representing tools that node MUST provide
 }
