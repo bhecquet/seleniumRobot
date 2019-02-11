c80904eb5466b715f098b11ae13b3f7b13062414
s047432 <s047432@SN782980.societe.mma.fr>
2019-02-11 15:01:08 +0100

    issue #206: add browserstack classes

---
+++ core/src/main/java/com/seleniumtests/connectors/selenium/BrowserStackGridConnector.java
@@ -0,0 +1,9 @@
+package com.seleniumtests.connectors.selenium;
+
+public class BrowserStackGridConnector extends SeleniumGridConnector {
+
+	public BrowserStackGridConnector(String url) {
+		super(url);
+	}
+
+}
