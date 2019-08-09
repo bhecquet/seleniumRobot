82088f14bfc46fd25894d5f98e5244e192dde8aa
bhecquet <bhecquet.w@free.fr>
2019-08-09 14:20:13 +0200

    issue #273: correct behaviour for unit tests from maven

--- core/src/test/java/com/seleniumtests/ut/uipage/TestPageObject.java
+++ core/src/test/java/com/seleniumtests/ut/uipage/TestPageObject.java
@@ -1,13 +1,26 @@
 package com.seleniumtests.ut.uipage;
 
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.seleniumtests.GenericTest;
+import com.seleniumtests.GenericDriverTest;
+import com.seleniumtests.core.SeleniumTestsContextManager;
 import com.seleniumtests.customexception.ConfigurationException;
 import com.seleniumtests.it.driver.support.pages.DriverTestPage;
+import com.seleniumtests.reporter.logger.TestLogging;
 
-public class TestPageObject extends GenericTest {
+public class TestPageObject extends GenericDriverTest {
 
+	@BeforeMethod(groups={"ut"})
+	public void initDriver(final ITestContext testNGCtx) throws Exception {
+
+		TestLogging.reset();
+		initThreadContext(testNGCtx);
+		SeleniumTestsContextManager.getThreadContext().setBrowser(null);
+		
+	}
+	
 	/**
 	 * issue #273: check we get a readable error message when browser type is not set
 	 * @throws Exception
