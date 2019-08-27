b97e405905725924a7d32874e55463981b581651
bhecquet <bhecquet.w@free.fr>
2019-08-27 12:05:18 +0200

    adding more logs

--- core/src/test/java/com/seleniumtests/it/driver/support/pages/DriverTestPageNativeActions.java
+++ core/src/test/java/com/seleniumtests/it/driver/support/pages/DriverTestPageNativeActions.java
@@ -47,6 +47,7 @@ public class DriverTestPageNativeActions extends PageObject {
     }
     
     public DriverTestPageNativeActions sendKeys() {
+    	System.out.println("source: " + driver.getPageSource());
     	driver.findElement(By.id("text2")).sendKeys("some text");
     	return this;
     }
