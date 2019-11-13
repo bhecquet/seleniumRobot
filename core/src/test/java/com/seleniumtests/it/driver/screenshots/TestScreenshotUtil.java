a37411965fb6aeeadc03a5c635563a7d2105ba56
bhecquet <bhecquet.w@free.fr>
2019-11-13 16:53:16 +0100

    issue #300: source and title are now get when capture is done

--- core/src/test/java/com/seleniumtests/it/driver/screenshots/TestScreenshotUtil.java
+++ core/src/test/java/com/seleniumtests/it/driver/screenshots/TestScreenshotUtil.java
@@ -17,14 +17,29 @@
  */
 package com.seleniumtests.it.driver.screenshots;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.testng.Assert;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.Test;
 import org.testng.xml.XmlSuite.ParallelMode;
 
+import com.seleniumtests.core.SeleniumTestsContext;
+import com.seleniumtests.core.SeleniumTestsContextManager;
+import com.seleniumtests.driver.TestType;
+import com.seleniumtests.driver.screenshots.ScreenShot;
+import com.seleniumtests.driver.screenshots.ScreenshotUtil;
+import com.seleniumtests.driver.screenshots.ScreenshotUtil.Target;
 import com.seleniumtests.it.reporter.ReporterTest;
 import com.seleniumtests.reporter.logger.Snapshot;
 import com.seleniumtests.reporter.logger.TestLogging;
@@ -56,5 +71,54 @@ public class TestScreenshotUtil extends ReporterTest {
 				}
 			}
 		}
+	}
+	
+	/**
+	 * issue #300: check that with multiple windows, we have all screenshots and the corresponding HTML code
+	 * @param testContext
+	 * @throws Exception
+	 */
+	@Test(groups={"it"})
+	public void testMultipleScreenshots(ITestContext testContext) throws Exception {
+		executeSubTest(1, new String[] {"com.seleniumtests.it.stubclasses.StubTestClassForDriverTest"}, ParallelMode.METHODS, new String[] {"testDriverMultipleSnapshot"});
+
+		FilenameFilter fileNameFilter = new FilenameFilter() {
+            public boolean accept( File dir, String name ) { 
+                return name.matches( ".*Test_end.*" );
+            }
+		};
+		
+		Path resultDir = Paths.get(SeleniumTestsContextManager.getGlobalContext().getOutputDirectory(), "testDriverMultipleSnapshot");
+		File[] htmlFiles = resultDir.resolve("htmls")
+			.toFile()
+			.listFiles(fileNameFilter);
+		Assert.assertEquals(htmlFiles.length, 2);
+		
+		// check that html files reflect the real window code
+		String mainWindowCode = FileUtils.readFileToString(htmlFiles[0]);
+		Assert.assertTrue(mainWindowCode.contains("<h3>Test clicking an element</h3>"));
+		Assert.assertFalse(mainWindowCode.contains("<a href=\"http://www.google.fr\" id=\"linkIFrame\" target=\"_blank\">My link in IFrame</a>"));
+		
+		String secondWindowCode = FileUtils.readFileToString(htmlFiles[1]);
+		Assert.assertFalse(secondWindowCode.contains("<h3>Test clicking an element</h3>"));
+		Assert.assertTrue(secondWindowCode.contains("<a href=\"http://www.google.fr\" id=\"linkIFrame\" target=\"_blank\">My link in IFrame</a>"));
+		
+		File[] imgFiles = resultDir.resolve("screenshots")
+				.toFile()
+				.listFiles(fileNameFilter);
+		Assert.assertEquals(imgFiles.length, 2);
+	}
+	
+	/**
+	 * issue #300: check that desktop capture is done
+	 * @param testContext
+	 * @throws Exception
+	 */
+	@Test(groups={"it"})
+	public void testDesktopScreenshots(ITestContext testContext) throws Exception {
+		SeleniumTestsContextManager.getThreadContext().setTestType(TestType.WEB);
+		ScreenShot screenshot = new ScreenshotUtil(null).capture(Target.SCREEN, ScreenShot.class);
+		Assert.assertTrue(new File(screenshot.getFullImagePath()).exists());
+		
 	}
 }
