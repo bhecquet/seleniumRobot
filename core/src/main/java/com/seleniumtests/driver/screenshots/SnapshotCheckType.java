c03cedb28319da83b461f3ed9b495343f4038c67
bhecquet <bhecquet.w@free.fr>
2020-03-18 17:39:06 +0100

    issue #332: change from enum to class to allow extensions

--- core/src/main/java/com/seleniumtests/driver/screenshots/SnapshotCheckType.java
+++ core/src/main/java/com/seleniumtests/driver/screenshots/SnapshotCheckType.java
@@ -1,7 +1,23 @@
 package com.seleniumtests.driver.screenshots;
 
-public enum SnapshotCheckType {
-
-	TRUE,		// snapshot will be compared to baseline if requested by option seleniumRobotServerCompareSnapshots
-	FALSE		// snapshot will not be compared
+/**
+ * Type of check we should do on snapshot
+ * @author s047432
+ *
+ */
+public class SnapshotCheckType {
+	
+	private boolean sendToServer;
+	
+	// snapshot will be compared to baseline if requested by option seleniumRobotServerCompareSnapshots
+	public static final SnapshotCheckType TRUE = new SnapshotCheckType(true);
+	
+	// snapshot will not be compared
+	public static final SnapshotCheckType FALSE = new SnapshotCheckType(false);
+			
+	private SnapshotCheckType(boolean sendToServer) {
+		this.sendToServer = sendToServer;
+	}
+		
+	// TODO: check without colors, check only part of the picture (exclusion zones defined directly in test)
 }
