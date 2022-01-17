695b4e8e380c46f20886b0692bdf1880143c719a
bhecquet <bhecquet.w@free.fr>
2022-01-17 10:52:01 +0100

    issue #467: add unit tests for ErrorCause

--- core/src/main/java/com/seleniumtests/core/testanalysis/ErrorCause.java
+++ core/src/main/java/com/seleniumtests/core/testanalysis/ErrorCause.java
@@ -1,13 +1,24 @@
 package com.seleniumtests.core.testanalysis;
 
+import com.seleniumtests.customexception.ScenarioException;
+import com.seleniumtests.reporter.logger.TestStep;
+
+import gherkin.StringUtils.ToString;
+
 public class ErrorCause {
 	
 	private ErrorType type;
 	private String description;
+	private TestStep testStep;
 
-	public ErrorCause(ErrorType type, String description) {
+	public ErrorCause(ErrorType type, String description, TestStep testStep) {
+		if (type == null) {
+			throw new ScenarioException("Test type cannot be null");
+		}
+		
 		this.type = type;
 		this.description = description;
+		this.testStep = testStep;
 	}
 
 	public ErrorType getType() {
@@ -16,6 +27,23 @@ public class ErrorCause {
 
 	public String getDescription() {
 		return description;
+	}
+	
+	public TestStep getTestStep() {
+		return testStep;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder message = new StringBuilder(type.toString());
+		if (description != null) {
+			message.append(": " + description);
+		}
+		if (testStep != null) {
+			message.append(String.format(" on step '%s'", testStep.getName()));
+		}
+		return message.toString();
 	}
 	
 	@Override
@@ -27,9 +55,15 @@ public class ErrorCause {
 		if (obj instanceof ErrorCause && this == obj) {
 			return true;
 		}
-		
+
 		ErrorCause cause = (ErrorCause)obj;
-		return cause.description != null && cause.description.equals(description) && cause.type == type;
+		
+		boolean sameDescription = (cause.description == null && description == null) || (cause.description != null && cause.description.equals(description));
+		boolean sameTestStep = (testStep == null && cause.testStep == null) || (cause.testStep != null && testStep != null && cause.testStep.getName().equals(testStep.getName()));
+		
+		return sameDescription
+				&& cause.type == type 
+				&& sameTestStep;
 			
 	}
 	
