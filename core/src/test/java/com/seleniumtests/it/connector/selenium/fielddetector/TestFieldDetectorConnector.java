2cfe28460ac8c933ab78325311ac080f533b02b9
bhecquet <bhecquet.w@free.fr>
2022-02-10 14:31:28 +0100

    issue #478: code cleaning

--- core/src/test/java/com/seleniumtests/it/connector/selenium/fielddetector/TestFieldDetectorConnector.java
+++ core/src/test/java/com/seleniumtests/it/connector/selenium/fielddetector/TestFieldDetectorConnector.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.testng.Assert;
 import org.testng.ITestContext;
 import org.testng.SkipException;
@@ -21,14 +19,6 @@ import com.seleniumtests.customexception.ConfigurationException;
 
 public class TestFieldDetectorConnector extends GenericTest {
 
-
-	private File createImageFromResource(String resource) throws IOException {
-		File tempFile = File.createTempFile("img", "." + FilenameUtils.getExtension(resource));
-		tempFile.deleteOnExit();
-		FileUtils.copyInputStreamToFile(Thread.currentThread().getContextClassLoader().getResourceAsStream(resource), tempFile);
-		
-		return tempFile;
-	}
 
 	@BeforeMethod(groups={"it"})
 	public void init(ITestContext ctx) {
