a551dd488656cfcd8a6cd5eb537335f3929204b5
bhecquet <bhecquet.w@free.fr>
2022-06-02 08:19:48 +0200

    #507: update connector

--- core/src/main/java/com/seleniumtests/browserfactory/BrowserStackCapabilitiesFactory.java
+++ core/src/main/java/com/seleniumtests/browserfactory/BrowserStackCapabilitiesFactory.java
@@ -17,16 +17,35 @@
  */
 package com.seleniumtests.browserfactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.http.auth.AuthenticationException;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import com.seleniumtests.core.SeleniumTestsContextManager;
 import com.seleniumtests.customexception.ConfigurationException;
 import com.seleniumtests.driver.DriverConfig;
 
-public class BrowserStackCapabilitiesFactory extends ICapabilitiesFactory {
+import io.appium.java_client.remote.MobileCapabilityType;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
+import kong.unirest.UnirestInstance;
+
+public class BrowserStackCapabilitiesFactory extends ICloudCapabilityFactory {
+	
+	private static final Pattern REG_USER_PASSWORD = Pattern.compile("https://([^:/]++):([^:@]++)@hub.browserstack.com/wd/hub");
+	private static final String BROWSERSTACK_UPLOAD_URL = "https://api-cloud.browserstack.com/app-automate/upload";
 	
     public BrowserStackCapabilitiesFactory(DriverConfig webDriverConfig) {
 		super(webDriverConfig);
 	}
+ 
 
 	@Override
     public DesiredCapabilities createCapabilities() {
@@ -38,23 +57,105 @@ public class BrowserStackCapabilitiesFactory extends ICapabilitiesFactory {
         String platformVersion = null;
         String platformName = null;
         
-        if (platform.toLowerCase().startsWith("windows")) {
-        	platformVersion = platform.toLowerCase().replace("windows", "").trim();
-        	platformName = "Windows";
-        } else if (platform.toLowerCase().startsWith("os x")) {
-        	platformVersion = platform.toLowerCase().replace("os x", "").trim();
-        	platformName = "OS X";
+        if (SeleniumTestsContextManager.isMobileTest()) {
+	        if(ANDROID_PLATFORM.equalsIgnoreCase(webDriverConfig.getPlatform())){
+		        
+	        	Capabilities androidCaps = new AndroidCapabilitiesFactory(webDriverConfig).createCapabilities();
+	        	capabilities.merge(androidCaps);
+
+	        	capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UIAutomator2");
+	            
+	        } else if (IOS_PLATFORM.equalsIgnoreCase(webDriverConfig.getPlatform())){
+	        	Capabilities iosCaps = new IOsCapabilitiesFactory(webDriverConfig).createCapabilities();
+	        	capabilities.merge(iosCaps);
+	        }
+	        
+	        capabilities.setCapability("device", webDriverConfig.getDeviceName()); // pour deviceName
+			capabilities.setCapability("realMobile", "true");
+			capabilities.setCapability("os_version", webDriverConfig.getMobilePlatformVersion());
+			capabilities.setCapability("os", webDriverConfig.getPlatform());
+        	
         } else {
-        	throw new ConfigurationException("Only Windows and Mac are supported desktop platforms ('Windows xxx' or 'OS X xxx'). See https://www.browserstack.com/automate/capabilities for details. 'platform' param is " + platform);
+        	if (platform.toLowerCase().startsWith("windows")) {
+	        	platformVersion = platform.toLowerCase().replace("windows", "").trim();
+	        	platformName = "Windows";
+	        	
+	        } else if (platform.toLowerCase().startsWith("os x")) {
+	        	platformVersion = platform.toLowerCase().replace("os x", "").trim();
+	        	platformName = "OS X";
+	        	
+	        } else {
+	        	throw new ConfigurationException("Only Windows and Mac are supported desktop platforms ('Windows xxx' or 'OS X xxx'). See https://www.browserstack.com/automate/capabilities for details. 'platform' param is " + platform);
+	        }
+        	
+        	if (webDriverConfig.getBrowserVersion() != null) {
+            	capabilities.setCapability("browser_version", webDriverConfig.getBrowserVersion());
+            }
+            capabilities.setCapability("os", platformName);
+            capabilities.setCapability("os_version", platformVersion); 
         }
         
-        capabilities.setCapability("browser", webDriverConfig.getBrowserType());
-        if (webDriverConfig.getBrowserVersion() != null) {
-        	capabilities.setCapability("browser_version", webDriverConfig.getBrowserVersion());
-        }
-        capabilities.setCapability("os", platformName);
-        capabilities.setCapability("os_version", platformVersion); 
+        capabilities.setCapability("browserName", webDriverConfig.getBrowserType());
+        capabilities.setCapability("name", SeleniumTestsContextManager.getThreadContext().getTestMethodSignature());
+        capabilities.setCapability("project", SeleniumTestsContextManager.getApplicationName());
+        
+        // we need to upload something
+ 		if (capabilities.getCapability(MobileCapabilityType.APP) != null) {
+ 			String appUrl = uploadFile((String)capabilities.getCapability(MobileCapabilityType.APP));
+ 			capabilities.setCapability("app", appUrl);
 
+ 		}
+        
         return capabilities;
+    }
+	
+	 /**
+     * Upload application to saucelabs server
+     * @param targetAppPath
+     * @param serverURL
+     * @return
+     * @throws IOException
+     * @throws AuthenticationException 
+     */
+    private String uploadFile(String application) {
+
+    	// extract user name and password from getWebDriverGrid
+    	Matcher matcher = REG_USER_PASSWORD.matcher(SeleniumTestsContextManager.getThreadContext().getWebDriverGrid().get(0));
+    	String user;
+    	String password;
+    	if (matcher.matches()) {
+    		user = matcher.group(1);
+    		password = matcher.group(2);
+    	} else {
+    		throw new ConfigurationException("webDriverGrid variable does not have the right format for connecting to sauceLabs: \"https://<user>:<token>@hub.browserstack.com/wd/hub\"");
+    	}
+    	
+    	try (UnirestInstance unirest = Unirest.spawnInstance();){
+    		
+    		String proxyHost = System.getProperty("https.proxyHost");
+    		String proxyPort = System.getProperty("https.proxyPort");
+    		if (proxyHost != null && proxyPort != null) {
+    			unirest.config().proxy(proxyHost, Integer.valueOf(proxyPort));
+    		}
+    		
+    		HttpResponse<JsonNode> jsonResponse = unirest.post(BROWSERSTACK_UPLOAD_URL)
+    					.basicAuth(user, password)
+    					.field("file", new File(application))
+    					.asJson()
+    					.ifFailure(response -> {
+    						throw new ConfigurationException(String.format("Application file upload failed: %s", response.getStatusText()));
+    						})
+    					.ifSuccess(response -> {
+    						logger.info("Application successfuly uploaded to Saucelabs");
+    						
+    					});
+    		return jsonResponse.getBody().getObject().getString("app_url");
+    		
+
+		} catch (UnirestException e) {
+			throw new ConfigurationException("Application file upload failed: " + e.getMessage());
+		}
+    	
+    	
     }
 }
