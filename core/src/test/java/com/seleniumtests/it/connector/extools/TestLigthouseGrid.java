d821b9dc1492d8a9f6bd3a3c652275bb35a33ab7
bhecquet <bhecquet.w@free.fr>
2022-08-26 14:13:14 +0200

    #514: add test for lighthouse in grid

---
+++ core/src/test/java/com/seleniumtests/it/connector/extools/TestLigthouseGrid.java
@@ -0,0 +1,46 @@
+package com.seleniumtests.it.connector.extools;
+
+import java.util.ArrayList;
+
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.SkipException;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.seleniumtests.connectors.extools.Lighthouse;
+import com.seleniumtests.connectors.extools.Lighthouse.Category;
+import com.seleniumtests.connectors.extools.LighthouseFactory;
+import com.seleniumtests.connectors.selenium.SeleniumGridConnector;
+import com.seleniumtests.connectors.selenium.SeleniumRobotGridConnector;
+import com.seleniumtests.driver.BrowserType;
+import com.seleniumtests.it.driver.support.GenericMultiBrowserTest;
+
+public class TestLigthouseGrid extends GenericMultiBrowserTest {
+
+	public TestLigthouseGrid() throws Exception {
+		super(BrowserType.CHROME, "DriverTestPage", true);
+	}
+	
+
+	@BeforeMethod(groups={"it"})
+	public void initConnector(ITestContext ctx) {
+		SeleniumGridConnector connector = new SeleniumRobotGridConnector("http://127.0.0.1:4444/wd/hub");
+		
+		if (!connector.isGridActive()) {
+			throw new SkipException("no seleniumrobot grid available");
+		}
+	}
+	
+	@Test(groups="it")
+	public  void testLighthouseExecution() {
+		Lighthouse lighthouseInstance = LighthouseFactory.getInstance();
+		lighthouseInstance.execute(testPageUrl, new ArrayList<>());
+		Assert.assertNotNull(lighthouseInstance.getHtmlReport());
+		Assert.assertNotNull(lighthouseInstance.getJsonReport());
+		Assert.assertNull(lighthouseInstance.getLogs());
+		Assert.assertTrue(lighthouseInstance.getScore(Category.ACCESSIBILITY) > 30);
+	}
+
+
+}
