32cb4a3de2fe28120f889f9e20c7fcf1171bbf20
Art Gane <67195863+ArtGane@users.noreply.github.com>
2022-12-12 08:21:35 -0500

    issue while we update a variable without a prefix it create one but method createOrUpdateVariable() works well (#533)

--- core/src/main/java/com/seleniumtests/core/SeleniumTestsContext.java
+++ core/src/main/java/com/seleniumtests/core/SeleniumTestsContext.java
@@ -1762,6 +1762,10 @@ public class SeleniumTestsContext {
     public SeleniumRobotVariableServerConnector getVariableServer() {
 		return variableServer;
 	}
+
+    public void setVariableServer(SeleniumRobotVariableServerConnector variableServer) {
+        this.variableServer = variableServer;
+    }
     
     public List<SeleniumGridConnector> getSeleniumGridConnectors() {
     	if (seleniumGridConnectors == null) {
