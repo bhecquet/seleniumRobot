f175edcd91e6e4327ff42ef26e253738e2200c23
bhecquet <bhecquet.w@free.fr>
2023-01-18 11:36:38 +0100

    #548: handle 'null' message of exceptions

--- core/src/main/java/com/seleniumtests/util/logging/ScenarioLogger.java
+++ core/src/main/java/com/seleniumtests/util/logging/ScenarioLogger.java
@@ -282,7 +282,7 @@ public class ScenarioLogger extends Logger {
 		if (throwable != null) {
 			StackTraceElement[] s1 = throwable.getStackTrace();
 			
-			StringBuilder string = new StringBuilder(throwable.getMessage());
+			StringBuilder string = new StringBuilder(throwable.getMessage() == null ? "null": throwable.getMessage());
 			for (int x = 0; x < s1.length; x++) {
 				try {
 					if (PageObject.class.isAssignableFrom(Class.forName(s1[x].getClassName()))) {
