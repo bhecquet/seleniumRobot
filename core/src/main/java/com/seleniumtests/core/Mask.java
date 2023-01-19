1003e3cac9c4bdbb703d324c17e03c68eaf8aeb1
bhecquet <bhecquet.w@free.fr>
2023-01-19 11:16:45 +0100

    #549: add missing file

---
+++ core/src/main/java/com/seleniumtests/core/Mask.java
@@ -0,0 +1,35 @@
+/**
+ * Orignal work: Copyright 2015 www.seleniumtests.com
+ * Modified work: Copyright 2016 www.infotel.com
+ * 				Copyright 2017-2019 B.Hecquet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.seleniumtests.core;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to document PageObject methods
+ *
+ */
+
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target(PARAMETER)
+public @interface Mask {
+
+
+}
