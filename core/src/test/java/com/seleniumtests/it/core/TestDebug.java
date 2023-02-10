e5558511e455d85544074da243f3d1a6498f05c7
bhecquet <bhecquet.w@free.fr>
2023-02-10 15:41:52 +0100

    correct UT
    if browser fails to load because of wrong driver, be sure to clean
    everything

--- core/src/test/java/com/seleniumtests/it/core/TestDebug.java
+++ core/src/test/java/com/seleniumtests/it/core/TestDebug.java
@@ -79,9 +79,7 @@ public class TestDebug extends GenericTest {
 		} catch (Exception e) {
 			logger.error(e);
 		} finally {
-			if (WebUIDriver.getWebDriver(false) != null) {
-				WebUIDriver.cleanUp();
-			}
+			WebUIDriver.cleanUp();
 		}
 	}
 	
