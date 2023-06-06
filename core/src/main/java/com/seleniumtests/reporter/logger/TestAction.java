ab85a3e89dcc6687bde91e55b9565e1d1e800900
ArtGane <argane.samson@gmail.com>
2023-06-06 10:35:00 +0200

    method for timestamp and modify template

--- core/src/main/java/com/seleniumtests/reporter/logger/TestAction.java
+++ core/src/main/java/com/seleniumtests/reporter/logger/TestAction.java
@@ -17,6 +17,7 @@
  */
 package com.seleniumtests.reporter.logger;
 
+import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,6 +50,7 @@ public class TestAction {
 	protected List<String> pwdToReplace;
 	protected boolean maskPassword = true;
 	protected boolean encoded = false;		// true if we have encoded messages
+	private LocalTime timestamp;
 	
 	/**
 	 * 
@@ -62,6 +64,9 @@ public class TestAction {
 		this.pwdToReplace = pwdToReplace.stream()
 					.filter(s -> s.length() > TestStepManager.MIN_PASSWORD_LENGTH)
 					.collect(Collectors.toList());
+
+
+		timestamp = getTimestamp();
 	}
 
 	/**
@@ -76,6 +81,10 @@ public class TestAction {
 			}
 		}
 		return newName;
+	}
+
+	public LocalTime getTimestamp() {
+		return LocalTime.now();
 	}
 
 	public Boolean getFailed() {
