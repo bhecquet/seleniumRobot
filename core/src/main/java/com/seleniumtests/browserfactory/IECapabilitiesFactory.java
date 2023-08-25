a2c47828695320502497f1d8bfd15d0f629ca882
bhecquet <bhecquet.w@free.fr>
2023-08-25 16:49:04 +0200

    #603: add capabilities for Selenium 3 so that it's compatible with the new driver

--- core/src/main/java/com/seleniumtests/browserfactory/IECapabilitiesFactory.java
+++ core/src/main/java/com/seleniumtests/browserfactory/IECapabilitiesFactory.java
@@ -61,6 +61,11 @@ public class IECapabilitiesFactory extends IDesktopCapabilityFactory {
         	// when attaching to an existing Internet Explorer, give the option to driver which will then not create a new Internet Explorer
 	        options.setCapability("attachExistingBrowser", true);
 			((Map<String, Object>) options.getCapability(SE_IE_OPTIONS)).put("attachExistingBrowser", true);
+			options.setCapability("ie.attachExistingBrowser", true);
+			((Map<String, Object>) options.getCapability(SE_IE_OPTIONS)).put("ie.attachExistingBrowser", true);
+			options.setCapability("ie.ignoreprocessmatch", true); // add this option (https://github.com/SeleniumHQ/selenium/pull/12246) because
+																						// without it, driver searches a process ID of -1
+			((Map<String, Object>) options.getCapability(SE_IE_OPTIONS)).put("ie.ignoreprocessmatch", true);
         }
         
         if (Boolean.TRUE.equals(webDriverConfig.getIeMode())) {
